3bb9679ba0ddcff0ffa3002a59a65466530d64da
Alexandr-web <alexandr.yakovlev228@gmail.com>
2023-08-07 17:10:16 +0300

    верстка страницы общая информация

--- resources/views/employee/documents.blade.php
+++ resources/views/employee/documents.blade.php
@@ -3,5 +3,54 @@
 @section('title', 'Общая информация')
 
 @section('content')
-    <h2>Общая информация</h2>
+    <div class="w-50 mx-auto">
+        <h2>Общая информация</h2>
+        <ul class="list-group">
+            <li class="list-group-item d-flex align-items-center justify-content-between">
+                <div class="w-50">
+                    Паспорт
+                </div>
+                <div class="w-50 d-flex align-items-center justify-content-end">
+                    <button class="btn btn-outline-secondary me-2">загрузить</button>
+                    <button class="btn btn-outline-warning">скачать</button>
+                </div>
+            </li>
+            <li class="list-group-item d-flex align-items-center justify-content-between">
+                <div class="w-50">
+                    Трудовой договор
+                </div>
+                <div class="w-50 d-flex align-items-center justify-content-end">
+                    <button class="btn btn-outline-secondary me-2">загрузить</button>
+                    <button class="btn btn-outline-warning">скачать</button>
+                </div>
+            </li>
+            <li class="list-group-item d-flex align-items-center justify-content-between">
+                <div class="w-50">
+                    Договор на оказание платных услуг
+                </div>
+                <div class="w-50 d-flex align-items-center justify-content-end">
+                    <button class="btn btn-outline-secondary me-2">загрузить</button>
+                    <button class="btn btn-outline-warning">скачать</button>
+                </div>
+            </li>
+            <li class="list-group-item d-flex align-items-center justify-content-between">
+                <div class="w-50">
+                    Ученический договор
+                </div>
+                <div class="w-50 d-flex align-items-center justify-content-end">
+                    <button class="btn btn-outline-secondary me-2">загрузить</button>
+                    <button class="btn btn-outline-warning">скачать</button>
+                </div>
+            </li>
+            <li class="list-group-item d-flex align-items-center justify-content-between">
+                <div class="w-50">
+                    Договор С3 на скидку
+                </div>
+                <div class="w-50 d-flex align-items-center justify-content-end">
+                    <button class="btn btn-outline-secondary me-2">загрузить</button>
+                    <button class="btn btn-outline-warning">скачать</button>
+                </div>
+            </li>
+        </ul>
+    </div>  
 @endsection
