cb4eac34e40811caae2db473554679c33b6428d9
Alexandr-web <alexandr.yakovlev228@gmail.com>
2023-08-08 16:56:36 +0300

    добавил таблицу на страницу students info

--- resources/views/employee/studentsInfo.blade.php
+++ resources/views/employee/studentsInfo.blade.php
@@ -3,5 +3,17 @@
 @section('title', 'Движение студентов')
 
 @section('content')
-<h2>Движение студентов</h2>
+<div class="d-flex align-items-center justify-content-between">
+    <h2>Движение студентов</h2>
+    <div class="d-flex align-items-center ms-3">
+        <button type="button" class="js-btn-download btn btn-outline-primary me-2">Скачать</button>
+        <button type="button" class="js-btn-save btn btn-outline-primary me-2">Сохранить</button>
+        <input class="js-upload-table-input form-control" type="file">
+    </div>
+</div>
+<table id="students-info-table" class="table table-bordered" border="1" cellspacing="0" cellpadding="0"></table>
+@endsection
+
+@section('scripts')
+    @vite(['resources/js/pages/employee/studentsInfo/index.js'])
 @endsection
