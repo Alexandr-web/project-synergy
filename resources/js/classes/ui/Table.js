ce1259e93f5ccbca753753037a3251862ae2d080
Alexandr-web <alexandr.yakovlev228@gmail.com>
2023-08-10 11:19:59 +0300

    сделал возможность изменять данные таблиц

--- resources/js/classes/ui/Table.js
+++ resources/js/classes/ui/Table.js
@@ -6,10 +6,21 @@ export default class Table {
         this.btnDownload = document.querySelector(selectorBtnDownload);
         this.btnSave = document.querySelector(selectorBtnSave);
         this.inputUpload = document.querySelector(selectorInputUpload);
+
+        this.selectorTable = selectorTable;
     }
 
     htmlTableToExcel() {
-        const file = xlsx.utils.table_to_book(this.table);
+        const cells = this.table.querySelectorAll("td");
+
+        cells.forEach((el) => {
+            el.removeAttribute("data-v");
+            el.removeAttribute("data-t");
+            el.removeAttribute("id");
+        });
+
+        const updatedTable = document.querySelector(this.selectorTable);
+        const file = xlsx.utils.table_to_book(updatedTable);
 
         xlsx.write(file, { bookType: "xlsx", bookSST: true, type: "base64", });
         xlsx.writeFile(file, "file.xlsx", { bookSST: true, type: "base64", });
@@ -27,6 +38,11 @@ export default class Table {
         const block = document.createElement("div");
 
         block.innerHTML = htmlResult;
+
+        const table = block.querySelector("table");
+        const cells = table.querySelectorAll("td");
+
+        cells.forEach((el) => el.setAttribute("contenteditable", ""));
 
         return block.querySelector("table");
     }
