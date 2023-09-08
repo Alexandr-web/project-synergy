69cb49edbf8fcd16c22a49159f7ea1317e234711
Alexandr-web <alexandr.yakovlev228@gmail.com>
2023-09-08 11:36:11 +0300

    доверстал фильтры

--- resources/views/components/search-student-form-component.blade.php
+++ resources/views/components/search-student-form-component.blade.php
@@ -14,7 +14,7 @@
             <form class="form filters__form">
                 <div class="form__field form__field--row">
                     <div class="form__field-title">Год поступления</div>
-                    <div class="form__field-content form__field-content--row">
+                    <div class="form__field-content">
                         <select class="form__select form__select--white" name="year-admission">
                             <option class="form__select-option" value="2023">2023</option>
                             <option class="form__select-option" value="2022">2022</option>
@@ -29,7 +29,7 @@
                     <div class="form__field-content form__field-content--row">
                         <label class="custom-checkbox" data-custom-checkbox-type="checkmark" for="is-foreigner">
                             <input class="form__checkbox" type="checkbox" id="is-foreigner" name="is-foreigner">
-                            <span class="custom-checkbox__target">
+                            <span class="custom-checkbox__target" style="background-image: url({{ asset('icons/check-off.svg') }})">
                                 <img class="custom-checkbox__icon" src="{{ asset('icons/check-on.svg') }}" alt="Галочка">
                             </span>
                         </label>
@@ -45,6 +45,54 @@
                         <label class="custom-radio" data-custom-radio-type="line" for="status-fired">
                             <input class="form__radio" type="radio" id="status-fired" value="fired" name="status">
                             <span class="custom-radio__target">Уволен</span>
+                        </label>
+                    </div>
+                </div>
+                <div class="form__field form__field--row">
+                    <div class="form__field-content">
+                        <div class="radios radios--column">
+                            <label class="custom-radio" data-custom-radio-type="box" for="bak">
+                                <input class="form__radio" type="radio" id="bak" value="БАК" name="education">
+                                <span class="custom-radio__target">Обучение на БАК (ВО)</span>
+                            </label>
+                            <label class="custom-radio" data-custom-radio-type="box" for="asp">
+                                <input class="form__radio" type="radio" id="asp" value="АСП" name="education">
+                                <span class="custom-radio__target">Обучение на АСП</span>
+                            </label>
+                            <label class="custom-radio" data-custom-radio-type="box" for="vvo">
+                                <input class="form__radio" type="radio" id="vvo" value="ВВО" name="education">
+                                <span class="custom-radio__target">Обучение на ВВО</span>
+                            </label>
+                            <label class="custom-radio" data-custom-radio-type="box" for="mva">
+                                <input class="form__radio" type="radio" id="mva" value="МВА" name="education">
+                                <span class="custom-radio__target">Обучение на МВА</span>
+                            </label>
+                            <label class="custom-radio" data-custom-radio-type="box" for="mag">
+                                <input class="form__radio" type="radio" id="mag" value="МАГ" name="education">
+                                <span class="custom-radio__target">Обучение на МАГ</span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="form__field">
+                    <div class="form__field-title m-b-10">Структурное подразделение</div>
+                    <div class="form__field-content">
+                        <select class="form__select form__select--white" name="subdivision">
+                            <option class="form__select-option" value="Структурное подразделение 1">Структурное подразделение 1</option>
+                            <option class="form__select-option" value="Структурное подразделение 1">Структурное подразделение 1</option>
+                            <option class="form__select-option" value="Структурное подразделение 1">Структурное подразделение 1</option>
+                            <option class="form__select-option" value="Структурное подразделение 1">Структурное подразделение 1</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="form__field form__field--row">
+                    <div class="form__field-title">Встреча проведена</div>
+                    <div class="form__field-content form__field-content--row">
+                        <label class="custom-checkbox" data-custom-checkbox-type="checkmark" for="meeting-is-finish">
+                            <input class="form__checkbox" type="checkbox" id="meeting-is-finish" name="is-complete">
+                            <span class="custom-checkbox__target" style="background-image: url({{ asset('icons/check-off.svg') }})">
+                                <img class="custom-checkbox__icon" src="{{ asset('icons/check-on.svg') }}" alt="Галочка">
+                            </span>
                         </label>
                     </div>
                 </div>
